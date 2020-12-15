a5cd9850c03f76434a9897744820f25da0962238
rojinakashefi <rojina.kashefi@yahoo.com>
2020-12-15 22:55:18 +0330

    Adding User Class and this getter setter methods

--- src/LoginForm.java
+++ src/LoginForm.java
@@ -49,4 +49,5 @@ public class LoginForm extends JFrame implements ActionListener {
             message.setText(" Invalid user.. ");
         }
     }
+
 }
---
+++ src/User.java
@@ -0,0 +1,45 @@
+public class User {
+
+    private long Id;
+    private String username;
+    private String password;
+
+    public User() {
+    }
+
+    public User(long id, String username, String password) {
+        Id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+}
