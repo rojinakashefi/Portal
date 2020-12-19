a68e79817350f9a6b3439a16232a6666000a6984
rojinakashefi <rojina.kashefi@yahoo.com>
2020-12-19 09:57:32 +0330

    Adding AllStudentForm and ProfessorInfo Form and completing admin menu action listener

--- src/AdminMenu.java
+++ src/AdminMenu.java
@@ -1,10 +1,23 @@
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 
 public class AdminMenu extends Menu {
+    private JButton weeklyMealBtn;
+    private JButton addProfessorBtn;
+    private JButton professorInfoBtn;
 
     AdminMenu(String username) {
         super(username, "Admin Menu", 6, 350);
-     }
+        weeklyMealBtn = new JButton(" Weekly Meal Plan ");
+        addProfessorBtn = new JButton(" Add Professor ");
+        professorInfoBtn = new JButton(" Professor Info ");
+        weeklyMealBtn.addActionListener(this);
+        addProfessorBtn.addActionListener(this);
+        professorInfoBtn.addActionListener(this);
+        this.panel.add(weeklyMealBtn);
+        this.panel.add(addProfessorBtn);
+        this.panel.add(professorInfoBtn);
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -15,6 +28,12 @@ public class AdminMenu extends Menu {
             new ChangeUsernameForm(getUsername());
         } else if (e.getSource() == changePasswordBtn) {
             new ChangePasswordForm(getUsername());
+        } else if (e.getSource() == weeklyMealBtn) {
+            new WeeklyMealPlanFormForAdmin();
+        } else if (e.getSource() == addProfessorBtn) {
+            new AddProfessorClassForm();
+        } else if (e.getSource() == professorInfoBtn) {
+            new ProfessorInfoForm();
         }
     }
 }
--- src/ProfessorInfoForm.java
+++ src/ProfessorInfoForm.java
@@ -5,23 +5,31 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-
-public class AllStudentsForm extends JFrame implements ActionListener {
-    private JPanel panel;
+public class ProfessorInfoForm extends JFrame implements ActionListener {
+    private JPanel panel,selectPanel;
+    private JLabel label;
     private JComboBox selectProfNameCombo;
     private JTable professorInfoTable;
     private JButton cancelBtn;
 
-    AllStudentsForm() {
+    ProfessorInfoForm() {
+        GridLayout gridLayout = new GridLayout(1,2);
+        gridLayout.setVgap(10);
+        label = new JLabel();
+        label.setText(" Select Professor ");
         selectProfNameCombo = new JComboBox();
+        selectPanel = new JPanel(gridLayout);
         professorInfoTable = new JTable();
-        String column_names[]= {"Student", "Course Name","Time Range"};
+        String column_names[]= {"Course Name","Time Range"};
         DefaultTableModel table_model = new DefaultTableModel(column_names,8);
         professorInfoTable =new JTable(table_model);
         cancelBtn = new JButton(" Cancel ");
         panel = new JPanel();
         BoxLayout boxLayout = new BoxLayout(panel, BoxLayout.Y_AXIS);
         panel.setLayout(boxLayout);
+        selectPanel.add(label);
+        selectPanel.add(selectProfNameCombo);
+        panel.add(selectPanel);
         panel.add(Box.createRigidArea(new Dimension(0, 20)));
         panel.add(new JScrollPane(professorInfoTable));
         panel.add(Box.createRigidArea(new Dimension(0, 20)));
@@ -31,7 +39,7 @@ public class AllStudentsForm extends JFrame implements ActionListener {
         cancelBtn.addActionListener(this);
         add(panel, BorderLayout.CENTER);
         setTitle("ADD PROFESSOR");
-        setSize(500, 500);
+        setSize(450, 310);
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
         setLocation(dim.width / 2 - this.getSize().width / 2, dim.height / 2 - this.getSize().height / 2);
         setVisible(true);
