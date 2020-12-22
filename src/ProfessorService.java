dc2ba9a88bd3c14df39ea17d93b3759971ccd1ca
rojinakashefi <rojina.kashefi@yahoo.com>
2020-12-22 17:06:48 +0330

    Adding remove class method to ProfessorService

--- src/ProfessorService.java
+++ src/ProfessorService.java
@@ -109,11 +109,50 @@ public class ProfessorService implements IProfessorService {
             }
         }
     }
-
     @Override
     public void remove(Class cls) {
-
+        String readableFileName = FileUtility.getReadableFileName(CLASS_FILE_NAME_First, CLASS_FILE_NAME_Second);
+        File f = new File(readableFileName);
+        if (f.exists()) {
+            ObjectInputStream ois = null;
+            ObjectOutputStream oos = null;
+            FileInputStream fis = null;
+            FileOutputStream fos = null;
+            try {
+                fis = new FileInputStream(readableFileName);
+                fos = new FileOutputStream(FileUtility.getWritableFileName(CLASS_FILE_NAME_First, CLASS_FILE_NAME_Second));
+                ois = new ObjectInputStream(fis);
+                oos = new ObjectOutputStream(fos);
+                while (true) {
+                    Class cla = (Class) ois.readObject();
+                    if (cla.getCourse().getCourseName().equalsIgnoreCase(cls.getCourse().getCourseName())
+                            && cla.getCourse().getProfessorName().equalsIgnoreCase(cls.getCourse().getProfessorName())
+                            && cla.getTimeRange() == cls.getTimeRange()
+                            && cla.getDay().equalsIgnoreCase(cls.getDay())) {
+                        //Do nothing
+                    } else {
+                        oos.writeObject(cla);
+                    }
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                //end of file exception -> remove main file and rename temp file
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    ois.close();
+                    fis.close();
+                    oos.close();
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
+
 
     @Override
     public void addProfessor(Professor professor) {
